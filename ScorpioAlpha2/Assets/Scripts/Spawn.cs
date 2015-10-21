4a4d56a7811cb0df263a03a4183594982601083a
The-O-King <techmanozmar@gmail.com>
2015-10-20 23:52:12 -0400

    Initial Dev Build
    Has mostly functioning "click to move" mechanic
    --Implement Coroutines for efficiency
    --Add Health to Player/Enemy
    --Change color of selected Unit
    --Cleanup Debug.log Calls in code

---
+++ ScorpioAlpha2/Assets/Scripts/Spawn.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawn : MonoBehaviour {
+
+	public GameObject infantryPrefab;
+	public GameObject armorPrefab;
+	public GameObject artilleryPrefab;
+	public GameObject gameManager;
+	public GameObject newUnit = null;
+	public bool targetFound = false;
+	
+	void Start(){
+		gameManager = GameObject.Find ("GameManager");
+	}
+
+	public void SpawnInfantry() {
+		newUnit = (GameObject)Instantiate(infantryPrefab, new Vector3(0, -50, 0), Quaternion.identity);
+		gameManager.GetComponent<GameManager>().selectedUnit = newUnit;
+	}
+
+	public GameObject findClosestSpawn() {
+		return GameObject.Find("Spawn1");
+	}
+
+
+
+}
